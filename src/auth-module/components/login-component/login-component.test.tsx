cc240ec337c318bf0017ce2d16ec6116a43617df
this4you <me_vlad@ukr.net>
2022-06-21 22:15:58 +0300

    TrelloC-0.2: add login component test logic

--- src/auth-module/components/login-component/login-component.test.tsx
+++ src/auth-module/components/login-component/login-component.test.tsx
@@ -1,3 +1,12 @@
-export default function() {
-    
-}
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Login from './login-component';
+
+describe("Login", () => {
+    it("render Login component", () => {
+        render(<Login/>)
+        expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
+        // screen.getByRole('img');
+    })
+});
